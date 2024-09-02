5632bf595ce273f02b51cd9ffdbf84f2acebbbf6
Paco Ríos <pacoriosgalan@gmail.com>
2024-09-02 19:17:08 +0200

    perf: :label: Add types and constans files. Remove not used code. Change title.

---
+++ fever-pets/src/types/index.ts
@@ -0,0 +1,11 @@
+export type Pet = {
+  id: number
+  name: string
+  kind: string
+  weight: number
+  height: number
+  length: number
+  photo_url: string
+  description: string
+  number_of_lives: number
+}
---
+++ fever-pets/src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const API_URL =
+  'https://my-json-server.typicode.com/Feverup/fever_pets_data/pets'
