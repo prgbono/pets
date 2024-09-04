82b78244e446587e7dacea8da686789c2ad609ac
Paco Ríos <pacoriosgalan@gmail.com>
2024-09-04 14:29:21 +0200

    perf: :zap: Fetch data in context provider so that sorting persist

--- fever-pets/src/components/Home.tsx
+++ fever-pets/src/components/Home.tsx
@@ -1,19 +1,10 @@
-import { useEffect, useState } from 'react'
-import { useFetch } from '../hooks/useFetch'
-import { Pet, SORTING_OPTIONS_TYPE } from '../types'
 import Footer from '../ui/Footer'
-import SortingBar from '../ui/SortingBar'
-import { API_URL } from '../utils/constants'
 import PetCard from './PetCard'
+import { usePetContext } from '../hooks/usePetContext'
+import SortingBar from '../ui/SortingBar'
 
 const Home = () => {
-  const { data, isLoading, hasError } = useFetch<Pet[]>(API_URL)
-
-  const [pets, setPets] = useState<Pet[]>(data ?? [])
-
-  useEffect(() => {
-    setPets(data ?? [])
-  }, [data])
+  const { pets, isLoading, handleSortOptionChange, hasError } = usePetContext()
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -21,32 +12,6 @@ const Home = () => {
 
   if (hasError) {
     return <div>Error: {hasError}</div>
-  }
-
-  const handleSortOptionChange = (option: SORTING_OPTIONS_TYPE) => {
-    setPets(
-      [...pets].sort((a: Pet, b: Pet) => {
-        switch (option) {
-          case 'Name':
-            return a.name.localeCompare(b.name)
-
-          case 'Kind':
-            return a.kind.localeCompare(b.kind)
-
-          case 'Weight':
-            return a.weight - b.weight
-
-          case 'Height':
-            return a.height - b.height
-
-          case 'Length':
-            return a.length - b.length
-
-          default:
-            return 0
-        }
-      })
-    )
   }
 
   function handlePetOfTheDay(): void {
@@ -61,7 +26,6 @@ const Home = () => {
       />
 
       <div className="row rows-cols-1 row-cols-md-3 g-3 pb-3">
-        {/* {data?.map((pet) => ( */}
         {pets.map((pet) => (
           <PetCard key={pet.id} {...pet} />
         ))}
