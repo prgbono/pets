bdd58aa8d0cb8689bcfab4e2811b802684d4ba9b
Paco Ríos <pacoriosgalan@gmail.com>
2024-09-05 03:29:38 +0200

    test: :test_tube: Testing config

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)']
+}
+
+export default config
