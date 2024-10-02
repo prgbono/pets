a334b4c1f1d1e3f823300b643fc6e09de1698ff2
Paco Ríos <pacoriosgalan@gmail.com>
2024-10-02 16:29:18 +0200

    ci(Config): :wrench: Add config for better organisation of imports by setting aliases '@'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/': path.resolve(__dirname, './src'),
+      '@/config': path.resolve(__dirname, './src/config'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/styles': path.resolve(__dirname, './src/styles'),
+      '@/utils': path.resolve(__dirname, './src/utils'),
+      '@/common': path.resolve(__dirname, './src/common'),
+      '@/assets': path.resolve(__dirname, './src/assets'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/routes': path.resolve(__dirname, './src/routes'),
+      '@/layouts': path.resolve(__dirname, './src/layouts'),
+      '@/hooks': path.resolve(__dirname, './src/hooks'),
+      '@/store': path.resolve(__dirname, './src/store'),
+      '@/api': path.resolve(__dirname, './src/api'),
+      '@/context': path.resolve(__dirname, './src/context'),
+      '@/i18n': path.resolve(__dirname, './src/i18n'),
+      '@/router': path.resolve(__dirname, './src/router'),
+      '@/types': path.resolve(__dirname, './src/types'),
+      '@/ui': path.resolve(__dirname, './src/ui')
+    }
+  }
 })
