fab070747e152fb820e4800958d6aa649b8007df
Paco Ríos <pacoriosgalan@gmail.com>
2024-10-29 12:16:37 +0100

    perf(Performance): :test_tube: Add defaultContext value for PetContext for avoiding errors
    Without a default value, access to PetContext from a component without Provider would provoke errors when try to execute methods like 'setPets' or read variables like 'currentPage'. They would be undefined

--- src/context/PetContext.ts
+++ src/context/PetContext.ts
@@ -1,4 +1,15 @@
+import { DEFAULT_ITEMS_PER_PAGE } from '@/utils/constants'
 import { PetContextType } from '@/types'
 import { createContext } from 'react'
 
-export const PetContext = createContext<PetContextType | []>([])
+const defaultPetContext: PetContextType = {
+  pets: [],
+  setPets: () => {},
+  currentPage: 1,
+  itemsPerPage: DEFAULT_ITEMS_PER_PAGE,
+  onSortOptionChange: () => {},
+  handleNextPage: () => {},
+  handlePrevPage: () => {}
+}
+
+export const PetContext = createContext<PetContextType | []>(defaultPetContext)
