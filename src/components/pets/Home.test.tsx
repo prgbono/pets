9ff334353e2ef351f07391c4d117155f17e39f6b
Paco Ríos <pacoriosgalan@gmail.com>
2024-10-29 11:12:33 +0100

    fix(Tests): :test_tube: Mock useFetch for not fetching real data in tests files
    mockedUseFetch type: Is used like this: (mockedUseFetch as jest.Mock) so that useFetch is set as a mock of Jest and can have the methods mockReturnValue and mockReturnValueOnce.
    
    This is just for mocking the custom hook. Tests that are failing will be fixed with next commits

--- src/components/pets/Home.test.tsx
+++ src/components/pets/Home.test.tsx
@@ -3,22 +3,37 @@ import { render, screen } from '@testing-library/react'
 import Home from './Home'
 import { MemoryRouter } from 'react-router-dom'
 import { PetContext } from '@/context/PetContext'
+import { useFetch as mockedUseFetch } from '@/hooks/useFetch'
 
+jest.mock('@/hooks/useFetch')
+
+const pets = [
+  {
+    id: 1,
+    name: 'mock name',
+    kind: 'dog',
+    weight: 7652,
+    height: 55,
+    length: 145,
+    photo_url: 'https://cdn2.thedogapi.com/images/BJT0Jx5Nm_1280.jpg',
+    description: 'Mocked description'
+  }
+]
 describe('Tests in <Home /> (PetContext)', () => {
-  const pets = [
-    {
-      id: 1,
-      name: 'mock name',
-      kind: 'dog',
-      weight: 7652,
-      height: 55,
-      length: 145,
-      photo_url: 'https://cdn2.thedogapi.com/images/BJT0Jx5Nm_1280.jpg',
-      description: 'Mocked description'
-    }
-  ]
+  beforeEach(() => {
+    ;(mockedUseFetch as jest.Mock).mockReturnValue({
+      data: pets,
+      isLoading: false,
+      hasError: null
+    })
+  })
 
   test('it must show Home with no pets', () => {
+    ;(mockedUseFetch as jest.Mock).mockReturnValueOnce({
+      data: [],
+      isLoading: false,
+      hasError: null
+    })
     render(
       <PetContext.Provider
         value={{
